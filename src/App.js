4484d2f1301c616caad795cdc612adfea4c947f5
Sreejita Dutta <sreejitadutta23@gmail.com>
2024-04-07 18:10:57 +0530

    hero section done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { styled, ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
 import {  BrowserRouter } from "react-router-dom";
+import Hero from "./components/sections/Hero";
 
 const Body= styled.div`
   background-color:  ${({ theme }) => theme.bg};
+  color:  ${({ theme }) => theme.text_primary };
   width: 100%;
   height: 90vh;
   overflow-x: hidden;
@@ -17,7 +19,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Body>
-          
+          <Hero/>
         </Body>
       </BrowserRouter>
     </ThemeProvider>
