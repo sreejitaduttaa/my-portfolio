8386597a012daed8bc1ca122696664d81fc3ad84
Sreejita Dutta <sreejitadutta23@gmail.com>
2024-04-07 21:25:00 +0530

    skills done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { darkTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
 import {  BrowserRouter } from "react-router-dom";
 import Hero from "./components/sections/Hero";
+import Skills from "./components/sections/Skills";
 
 const Body= styled.div`
   background-color:  ${({ theme }) => theme.bg};
@@ -20,6 +21,7 @@ function App() {
         <Navbar />
         <Body>
           <Hero/>
+          <Skills/>
         </Body>
       </BrowserRouter>
     </ThemeProvider>
